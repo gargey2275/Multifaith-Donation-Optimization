f095a82d55ec609af2804e3580738fa4e6d3f49e
Gargey Ajay Mahajan <mahajangargey2005@gmail.com>
2025-10-07 19:43:55 +0530

    Remove dotenv from production

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
+//require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
